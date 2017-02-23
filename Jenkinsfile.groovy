bd992c06816828f50365d38cd762f3e5e0940f28
Theodorus Yoga <theodorusyoga_ti09@mail.ugm.ac.id>
2017-02-23 16:31:57 +0700

    edit again

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ import groovy.json.JsonSlurperClassic
 node {
     env.AWS_DEFAULT_REGION = 'ap-southeast-1'
 	
-	def applicationName = 'theodorus-app'
+	def applicationName = 'hendrawan-app'
 	def deploymentGroupName = 'theodorus-app-group' // change me
 	def s3BucketName = 'deployment-cdc'
 	
